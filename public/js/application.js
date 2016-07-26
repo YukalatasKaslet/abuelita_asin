057bcb928c10d45bde83914f2e3e218fe93b3431
codeamac10 <codeamac10@gmail.com>
2016-07-26 16:25:30 -0500

    Comentado y funcionando al 100% :D

--- public/js/application.js
+++ public/js/application.js
@@ -4,45 +4,50 @@ $(document).ready(function() {
 	// Esto garantiza que si amarramos(bind) una función a un elemento 
 	// de HTML este exista ya en la página. 
   $('form').submit(function(event){
-      event.preventDefault();
-      var info = $(this).serialize();
-      console.log( info );
-      /*var posting = $.post( "/abuelita" ,info, function(data) {
-            alert( "success:" + data  );
-            
-          })
-            .done(function(data) {
-              alert( "second success:" + data );
-              //cochinadas xD
-              //var content = $(data).find("#content");
-              //$("#grandma_says").empty().append( data );
-            })
-            .fail(function() {
-              alert( "error" );
-            })
-            .always(function() {
-              alert( "finished" );
-      );
-      posting.done(function(data) {
-        console.log(data);
-        $( "#test" ).append( data );
-          // var content = $( data ).find( "#content" );
-          // $( "#grandma_says" ).empty().append( content );
-      });
-/*    
-    $.ajax({
-      type: "POST",
-      url: "/abuelita",
-      data: user
+    event.preventDefault();
+    var info = $(this).serialize();
+    console.log( info );
+      //primera forma de resolver el problema :D
+      /*    $.post( "/abuelita" ,info, function(data) {
+                alert( "success:" + data  );
+                
+              })
+                .done(function(data) {
+                  alert( "second success:" + data );
+                  //cochinadas xD
+                  //var content = $(data).find("#content"); //no es necesario
+                  //$("#answer").empty().append( data );  //funciona
+                })
+                .fail(function() {
+                  alert( "error" );
+                })
+                .always(function() {
+                  alert( "finished" );
+            );
 
+      //Segunda forma de resolver el problema :3
+            var posting = $.post( "/abuelita" ,info);
+            posting.done(function(data) {
+              console.log(data);
+              $( "#answer" ).append( data );
+                // var content = $( data ).find( "#content" ); //no es necesario
+                // $( "#grandma_says" ).empty().append( content ); //se uso id answer
+            });
+
+      //Tercera forma de resolver el problema :P --propuesta--  no corre por si solo INCOMPLETO   
+            $.ajax({
+              type: "POST",
+              url: "/abuelita",
+              data: user
+        
+            });
+      */ 
+      //Mejor forma de resolver el problema :3     
+    $.post('/abuelita', info, function(data){
+      console.log(data);
+      $( "#answer" ).empty().append( data );//importante usar .empty o las respuestas se acumulan
     });
-*/      
-  $.post('/abuelita', info, function(data){
-    console.log(data);
-    $( "#test" ).append( data );
-  });
 
+  });//end $(form).submit
 
-  });
-
-});
+});//end document ready
