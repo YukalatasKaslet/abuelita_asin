e36ea6dfa35b4682d21f01782df01c48de218ec9
codeamac10 <codeamac10@gmail.com>
2016-07-26 13:17:28 -0500

    Post hecho, falta mostrar respuesta

--- public/js/application.js
+++ public/js/application.js
@@ -3,5 +3,32 @@ $(document).ready(function() {
 	// completamente. 
 	// Esto garantiza que si amarramos(bind) una función a un elemento 
 	// de HTML este exista ya en la página. 
+  $('form').submit(function(event){
+      event.preventDefault();
+      var user = $(this).serialize();
+      console.log( user );
+      $.post( "/abuelita" ,user, function() {
+            alert( "success" );
+          })
+            .done(function() {
+              alert( "second success" );
+            })
+            .fail(function() {
+              alert( "error" );
+            })
+            .always(function() {
+              alert( "finished" );
+      });
+    
+/*    
+    $.ajax({
+      type: "POST",
+      url: "/abuelita",
+      data: user
+
+    });
+*/      
+
+  });
 
 });
