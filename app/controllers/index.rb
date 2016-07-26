f34e4545e9ca5c5f834bdbe8e19040520fabba13
codeamac10 <codeamac10@gmail.com>
2016-07-26 10:22:54 -0500

    index.rb modificado para que identifique AJAX

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,13 +9,27 @@ end
 post '/abuelita' do
   user_input = params[:user_input]
 
-  if user_input == "BYE"
-    redirect to('/?abuelita="Bye m\'ijito,TQM"')
-  elsif user_input == user_input.upcase
-    redirect to('/?abuelita="NO, NO DESDE 1983"')
-  elsif user_input == user_input.downcase
-    redirect to('/?abuelita="HUH?! NO TE ESCUCHO, HIJO!"')
-  
+  if request.xhr?
+    #respond to Ajax request
+    if user_input == "BYE"
+      "Bye m\'ijito,TQM"
+    elsif user_input == user_input.upcase
+      "NO, NO DESDE 1983"
+    elsif user_input == user_input.downcase
+      "HUH?! NO TE ESCUCHO, HIJO!"
+    
+    end
+
+  else
+    #respond to normal request
+    if user_input == "BYE"
+      redirect to('/?abuelita="Bye m\'ijito,TQM"')
+    elsif user_input == user_input.upcase
+      redirect to('/?abuelita="NO, NO DESDE 1983"')
+    elsif user_input == user_input.downcase
+      redirect to('/?abuelita="HUH?! NO TE ESCUCHO, HIJO!"')
+    
+    end
   end
 
 end
